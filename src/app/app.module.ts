5d5fffae06464f8cb1f7dbe1ea643d4eb9b4a6c5
Darling Prasanth <prasanth.maddala@chromiumsolutions.com>
2022-02-15 14:42:58 +0530

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,82 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { BlckBlockComponent } from './blck-block/blck-block.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { TouchComponent } from './touch/touch.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
+    AppComponent,
+    BlckBlockComponent,
+    ToolbarComponent,
+    TouchComponent 
+  ], 
+  imports: [ 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule, 
+    MatCheckboxModule, 
+    MatChipsModule, 
+    MatDatepickerModule, 
+    MatDialogModule, 
+    MatDividerModule,
+    MatExpansionModule, 
+    MatFormFieldModule, 
+    MatIconModule, 
+    MatInputModule, 
+    MatListModule, 
+    MatMenuModule,
+    MatSelectModule, 
+    MatSidenavModule, 
+    MatSlideToggleModule, 
+    MatSliderModule, 
+    MatSnackBarModule,
+    MatSortModule, 
+    MatStepperModule, 
+    MatTableModule,
+    MatTabsModule, 
+    MatToolbarModule, 
+    MatTooltipModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/blck-block/blck-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blck-block',
+  templateUrl: './blck-block.component.html',
+  styleUrls: ['./blck-block.component.scss']
+})
+export class BlckBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
